0f390ca910cf04e6af11518dc668135ded843394
vince89 <vincent26jecs004@gmail.com>
2015-10-06 00:08:17 +0800

    test case solved

--- test/AddMenuTest.java
+++ test/AddMenuTest.java
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.coffeeshop.service.OrderService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/WegoShop-servlet.xml"})
+public class AddMenuTest {
+	@Autowired
+	private OrderService orderService ;
+=======
 import static org.junit.Assert.*;
 
 import org.junit.After;
@@ -6,6 +23,7 @@ import org.junit.Test;
 
 
 public class AddMenuTest {
+>>>>>>> c98c97c91132436727b53420a2b8103e121255ac
 
 	@Before
 	public void setUp() throws Exception {
@@ -17,7 +35,12 @@ public class AddMenuTest {
 
 	@Test
 	public void testInsertOrder() {
+<<<<<<< HEAD
+		orderService.AddMenu();
+
+=======
 		fail("Not yet implemented");
+>>>>>>> c98c97c91132436727b53420a2b8103e121255ac
 	}
 
 }
